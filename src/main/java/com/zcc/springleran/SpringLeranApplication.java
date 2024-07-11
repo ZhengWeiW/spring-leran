2a12009d477c462552f029a448a02150a389aab4
郑韦伟 <zhengww1@akulaku.com>
2024-07-11 18:09:16 +0800

    第一次提交

---
+++ src/main/java/com/zcc/springleran/SpringLeranApplication.java
@@ -0,0 +1,13 @@
+package com.zcc.springleran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLeranApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLeranApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zcc/springleran/SpringLeranApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zcc.springleran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLeranApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
