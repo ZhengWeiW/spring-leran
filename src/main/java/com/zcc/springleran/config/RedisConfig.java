5d340031e1fe074b8610ce93f4aee56fbd5e0ca1
郑韦伟 <zhengww1@akulaku.com>
2024-07-19 12:11:50 +0800

    演示Aop

---
+++ src/main/java/com/zcc/springleran/config/RedisConfig.java
@@ -0,0 +1,43 @@
+package com.zcc.springleran.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+/**
+ * @description:
+ * @author: zcc
+ * @createDate: 2024/7/19
+ * @version: 1.0
+ */
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(host);
+        config.setPort(port);
+        config.setPassword(RedisPassword.of(password));
+        return new LettuceConnectionFactory(config);
+    }
+    @Bean
+    public ThreadLocal<RedisConnection> redisConnectionThreadLocal(RedisConnectionFactory redisConnectionFactory) {
+        return ThreadLocal.withInitial(() -> redisConnectionFactory.getConnection());
+    }
+}
+
